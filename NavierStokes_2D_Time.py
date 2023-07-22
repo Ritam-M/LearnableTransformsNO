3ca9512e00159b0fdd29d1a4501ec62311c0a3af
Ritam Majumdar <hrickmajumdar@gmail.com>
2023-07-22 19:49:54 +0530

    Create NavierStokes_2D_Time.py

---
+++ NavierStokes_2D_Time.py
@@ -0,0 +1,239 @@
+import torch.nn.functional as F
+from utilities import *
+from timeit import default_timer
+
+torch.manual_seed(0)
+np.random.seed(0)
+
+class NeuralConv2d(nn.Module):
+    def __init__(self, in_channels, out_channels, dims, modes1, modes2):
+        super(SpectralConv2d, self).__init__()
+
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.dims = dims
+        self.modes1 = modes1 
+        self.modes2 = modes2
+
+        self.scale = (1 / (in_channels * out_channels))
+        self.weights1 = nn.Parameter(self.scale * torch.rand(in_channels, out_channels, self.modes1, self.modes2))
+        self.weights2 = nn.Parameter(self.scale * torch.rand(in_channels, out_channels, self.modes1, self.modes2))
+        
+        self.M1 = nn.Linear(self.dims[0],self.modes1)
+        self.M2 = nn.Linear(self.dims[1],self.modes2)
+        
+        self.N1 = nn.Linear(self.modes1,self.dims[0])
+        self.N2 = nn.Linear(self.modes2,self.dims[1])
+
+    def M(self, input, weights):
+        # (batch, in_channel, x,y ), (in_channel, out_channel, x,y) -> (batch, out_channel, x,y)
+        return torch.einsum("bixy,ioxy->boxy", input, weights)
+
+    def forward(self, x):
+
+        batchsize = x.shape[0]
+        x_ft = self.M1(x.permute(0,1,3,2)).permute(0,1,2,3)
+        x_ft = self.M2(x_ft.permute(0,1,3,2))
+        
+        out_ft = self.R(x_ft, self.weights1)
+        out_ft = self.R(out_ft, self.weights2)
+        
+        x = self.N1(out_ft.permute(0,1,3,2)).permute(0,1,2,3)
+        x = self.N2(x.permute(0,1,3,2))
+      
+        return x
+      
+class MLP(nn.Module):
+    def __init__(self, in_channels, out_channels, mid_channels):
+        super(MLP, self).__init__()
+        self.mlp1 = nn.Conv2d(in_channels, mid_channels, 1)
+        self.mlp2 = nn.Conv2d(mid_channels, out_channels, 1)
+
+    def forward(self, x):
+        x = self.mlp1(x)
+        x = F.gelu(x)
+        x = self.mlp2(x)
+        return x
+
+class NO2d(nn.Module):
+    def __init__(self, modes1, modes2, dims, width):
+        super(NO2d, self).__init__()
+
+        self.modes1 = modes1
+        self.modes2 = modes2
+        self.dims = dims
+        self.width = width
+        self.padding = 8 # pad the domain if input is non-periodic
+
+        self.p = nn.Linear(12, self.width) # input channel is 12: the solution of the previous 10 timesteps + 2 locations (u(t-10, x, y), ..., u(t-1, x, y),  x, y)
+        self.conv0 = NeuralConv2d(self.width, self.width, self.dims, self.modes1, self.modes2)
+        self.conv1 = NeuralConv2d(self.width, self.width, self.dims, self.modes1, self.modes2)
+        self.conv2 = NeuralConv2d(self.width, self.width, self.dims, self.modes1, self.modes2)
+        self.conv3 = NeuralConv2d(self.width, self.width, self.dims, self.modes1, self.modes2)
+        self.mlp0 = MLP(self.width, self.width, self.width)
+        self.mlp1 = MLP(self.width, self.width, self.width)
+        self.mlp2 = MLP(self.width, self.width, self.width)
+        self.mlp3 = MLP(self.width, self.width, self.width)
+        self.w0 = nn.Conv2d(self.width, self.width, 1)
+        self.w1 = nn.Conv2d(self.width, self.width, 1)
+        self.w2 = nn.Conv2d(self.width, self.width, 1)
+        self.w3 = nn.Conv2d(self.width, self.width, 1)
+        self.q = MLP(self.width, 1, self.width * 4) # output channel is 1: u(x, y)
+
+    def forward(self, x):
+        grid = self.get_grid(x.shape, x.device)
+        x = torch.cat((x, grid), dim=-1)
+        x = self.p(x)
+        x = x.permute(0, 3, 1, 2)
+        x = F.pad(x, [0,self.padding, 0,self.padding])
+
+        x1 = self.conv0(x)
+        x1 = self.mlp0(x1)
+        x2 = self.w0(x)
+        x = x1 + x2
+        x = F.gelu(x)
+
+        x1 = self.conv1(x)
+        x1 = self.mlp1(x1)
+        x2 = self.w1(x)
+        x = x1 + x2
+        x = F.gelu(x)
+
+        x1 = self.conv2(x)
+        x1 = self.mlp2(x1)
+        x2 = self.w2(x)
+        x = x1 + x2
+        x = F.gelu(x)
+
+        x1 = self.conv3(x)
+        x1 = self.mlp3(x1)
+        x2 = self.w3(x)
+        x = x1 + x2
+
+        x = x[..., :-self.padding, :-self.padding]
+        x = self.q(x)
+        x = x.permute(0, 2, 3, 1)
+        return x
+    
+    def get_grid(self, shape, device):
+        batchsize, size_x, size_y = shape[0], shape[1], shape[2]
+        gridx = torch.tensor(np.linspace(0, 1, size_x), dtype=torch.float)
+        gridx = gridx.reshape(1, size_x, 1, 1).repeat([batchsize, 1, size_y, 1])
+        gridy = torch.tensor(np.linspace(0, 1, size_y), dtype=torch.float)
+        gridy = gridy.reshape(1, 1, size_y, 1).repeat([batchsize, size_x, 1, 1])
+        return torch.cat((gridx, gridy), dim=-1).to(device)
+
+################################################################
+# configs
+################################################################
+
+TRAIN_PATH = 'data/ns_data_V100_N1000_T50_1.mat'
+TEST_PATH = 'data/ns_data_V100_N1000_T50_2.mat'
+
+ntrain = 1000
+ntest = 200
+
+modes = 12
+width = 32
+
+batch_size = 20
+learning_rate = 0.001
+epochs = 500
+iterations = epochs*(ntrain//batch_size)
+
+sub = 1
+S = 64
+T_in = 10
+T = 10 
+step = 1
+
+################################################################
+# load data
+################################################################
+
+reader = MatReader(TRAIN_PATH)
+train_a = reader.read_field('u')[:ntrain,::sub,::sub,:T_in]
+train_u = reader.read_field('u')[:ntrain,::sub,::sub,T_in:T+T_in]
+
+reader = MatReader(TEST_PATH)
+test_a = reader.read_field('u')[-ntest:,::sub,::sub,:T_in]
+test_u = reader.read_field('u')[-ntest:,::sub,::sub,T_in:T+T_in]
+
+print(train_u.shape)
+print(test_u.shape)
+assert (S == train_u.shape[-2])
+assert (T == train_u.shape[-1])
+
+train_a = train_a.reshape(ntrain,S,S,T_in)
+test_a = test_a.reshape(ntest,S,S,T_in)
+
+train_loader = torch.utils.data.DataLoader(torch.utils.data.TensorDataset(train_a, train_u), batch_size=batch_size, shuffle=True)
+test_loader = torch.utils.data.DataLoader(torch.utils.data.TensorDataset(test_a, test_u), batch_size=batch_size, shuffle=False)
+
+################################################################
+# training and evaluation
+################################################################
+model = NO2d(modes, modes, [S,S], width).cuda()
+print(count_params(model))
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate, weight_decay=1e-4)
+scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=iterations)
+
+myloss = LpLoss(size_average=False)
+for ep in range(epochs):
+    model.train()
+    t1 = default_timer()
+    train_l2_step = 0
+    train_l2_full = 0
+    for xx, yy in train_loader:
+        loss = 0
+        xx = xx.to(device)
+        yy = yy.to(device)
+
+        for t in range(0, T, step):
+            y = yy[..., t:t + step]
+            im = model(xx)
+            loss += myloss(im.reshape(batch_size, -1), y.reshape(batch_size, -1))
+
+            if t == 0:
+                pred = im
+            else:
+                pred = torch.cat((pred, im), -1)
+
+            xx = torch.cat((xx[..., step:], im), dim=-1)
+
+        train_l2_step += loss.item()
+        l2_full = myloss(pred.reshape(batch_size, -1), yy.reshape(batch_size, -1))
+        train_l2_full += l2_full.item()
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        scheduler.step()
+
+    test_l2_step = 0
+    test_l2_full = 0
+    with torch.no_grad():
+        for xx, yy in test_loader:
+            loss = 0
+            xx = xx.to(device)
+            yy = yy.to(device)
+
+            for t in range(0, T, step):
+                y = yy[..., t:t + step]
+                im = model(xx)
+                loss += myloss(im.reshape(batch_size, -1), y.reshape(batch_size, -1))
+
+                if t == 0:
+                    pred = im
+                else:
+                    pred = torch.cat((pred, im), -1)
+
+                xx = torch.cat((xx[..., step:], im), dim=-1)
+
+            test_l2_step += loss.item()
+            test_l2_full += myloss(pred.reshape(batch_size, -1), yy.reshape(batch_size, -1)).item()
+
+    t2 = default_timer()
+    print(ep, t2 - t1, train_l2_step / ntrain / (T / step), train_l2_full / ntrain, test_l2_step / ntest / (T / step),
+          test_l2_full / ntest)
+# torch.save(model, path_model)
